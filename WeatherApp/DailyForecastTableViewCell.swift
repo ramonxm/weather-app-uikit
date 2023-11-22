07c5b9b127afd5e78b47f810fafbeb7832127deb
Ramon Xavier <ramon1234rxm@gmail.com>
2023-11-21 21:24:58 -0300

    feat: adding vector and uiview extensions

---
+++ WeatherApp/DailyForecastTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  DailyForecastTableViewCell.swift
+//  WeatherApp
+//
+//  Created by Ramon Xavier on 21/11/23.
+//
+
+import UIKit
+
+class DailyForecastTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ WeatherApp/UIView+Extensions.swift
@@ -0,0 +1,20 @@
+//
+//  UIView+Extensions.swift
+//  WeatherApp
+//
+//  Created by Ramon Xavier on 21/11/23.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    func setConstraintsToParent(_ parent: UIView) {
+        NSLayoutConstraint.activate([
+            self.topAnchor.constraint(equalTo: parent.topAnchor),
+            self.leadingAnchor.constraint(equalTo: parent.leadingAnchor),
+            self.trailingAnchor.constraint(equalTo: parent.trailingAnchor),
+            self.bottomAnchor.constraint(equalTo: parent.bottomAnchor)
+        ])
+    }
+}
